d8bf7b8421e6ec219dbf76c92fe4e9ff3a5c0d5f
Max Martinsen <111891491+MaxMartinsen@users.noreply.github.com>
2023-09-09 15:16:25 +0200

    Update exercise-3.js

--- map/exercise-3.js
+++ map/exercise-3.js
@@ -22,16 +22,20 @@ https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
 
 const buttons = [
   {
-    text: 'Button Text 1',
+    text: "Button Text 1",
   },
   {
-    text: 'Button Text 2',
+    text: "Button Text 2",
   },
   {
-    text: 'Button Text 3',
+    text: "Button Text 3",
   },
 ];
 
-const buttonElements = null; // Replace null and add .map code here
+const buttonElements = buttons.map((button) => {
+  const btnElement = document.createElement("button");
+  btnElement.innerText = button.text;
+  return btnElement;
+}); // Replace null and add .map code here
 
 console.log(buttonElements);
